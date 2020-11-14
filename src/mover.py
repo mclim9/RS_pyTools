5cb1491af3aff8a77ea848911258f4c7fa38193f
Martin Lim <trabin9@gmail.com>
2020-11-13 21:00:55 -0800

    typo

--- src/mover.py
+++ src/mover.py
@@ -7,4 +7,4 @@ for i in range(hours*minutes):
     pyautogui.move(10,10)
     pyautogui.move(-10,-10)
     time.sleep(60)
-    print(f'{time.strftime("%Y.%m.%d %H:%M:%S")} i:{i of {hours*minutes}}')
+    print(f'{time.strftime("%Y.%m.%d %H:%M:%S")} i:{i} of {hours*minutes}')
