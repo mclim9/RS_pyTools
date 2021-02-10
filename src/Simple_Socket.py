f7e8b9fda4307ef63c3986a9ba9dbc2c41cd9172
Martin Lim <trabin9@gmail.com>
2021-02-10 08:55:11 -0800

    del getSysInfo

--- src/Simple_Socket.py
+++ src/Simple_Socket.py
@@ -1,42 +1,34 @@
 """Rohde & Schwarz Automation for demonstration use. """
 #pylint: disable=invalid-name
 
-import socket                                           #Import socket module
-import xml.etree.ElementTree as ET
-host = '192.168.1.109'                                  #Instrument IP address
+import socket                                  # Import socket module
 
 ###############################################################################
 ### Code Begin
 ###############################################################################
-
 def sQuery(SCPI):
-    '''socket query'''
+    """Socket Query"""
+    print(f'Write: {SCPI}')
     out = SCPI + "\n"
-    s.sendall(out.encode())                             #Write 'cmd'
-    sOut = s.recv(2048).strip()                         #read socket
-    return sOut.decode()
+    s.sendall(out.encode())                     # Write 'cmd'
+    sOut = s.recv(2048).strip()                 # read socket
+    sOut = sOut.decode()
+    print(f'Query: {sOut}')
+    return sOut
 
 def sWrite(SCPI):
-    '''socket write'''
+    """Socket Write"""
+    print(f'Write: {SCPI}')
     out = SCPI + "\n"
-    s.sendall(out.encode())                             #Write 'cmd'
+    s.sendall(out.encode())                     # Write 'cmd'
 
-def getSysInfo():
-    '''get system options'''
-    xmlIn = sQuery("SYST:DFPR?")
-    strStart = xmlIn.find('deviceId="') + len('deviceID="')
-    strStop  = xmlIn.find('type="') - 2
-    xmlIn = xmlIn[strStart:strStop]                     #Remove header
-    print(xmlIn)
-    return xmlIn
 
-###############################################################################
-### Main Code
-###############################################################################
-s = socket.socket()                                     # Create a socket object
-s.connect((host, 5025))
-s.settimeout(1)                                         # Timeout in seconds
+# ##############################################################################
+# ## Main Code
+# ##############################################################################
+s = socket.socket()                             # Create a socket object
+s.connect(('192.168.58.109', 5025))
+s.settimeout(1)                                 # Timeout in seconds
 
-print("Info:" + sQuery("*IDN?"))
-print("Opts:" + sQuery("*OPT?"))
-getSysInfo()
+print("Info : " + sQuery("*IDN?"))
+print("Opts : " + sQuery("*OPT?"))
