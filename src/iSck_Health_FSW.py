44eb720231f1a0327a7e5f08272989d42c6d0880
Martin Lim <trabin9@gmail.com>
2021-03-09 14:59:19 -0800

    update instrument Health check.

--- src/iSck_Health_FSW.py
+++ src/iSck_Health_FSW.py
@@ -6,6 +6,22 @@ from iSocket import iSocket
 # #############################################################################
 if __name__ == "__main__":
     s = iSocket().open('192.168.58.109', 5025)
+    print('\n--HW Info--')
+    HWInfo = s.query('DIAG:SERV:HWIN?').split('","')
+    for module in HWInfo:
+        if 'DETECTOR EXTENSION BOARD 2' in module:
+            print(f'Detect : {module}')
+        if 'FSW DEVICE' in module:
+            print(f'Model  : {module}')
+
+    print('\n--Self Test Info--')
+    selfTest = s.query('DIAG:SERV:STES:RES?').split(',')
+    print(f"State  : {selfTest[0]}")
+    print(f"Firmwar: {selfTest[1]}")
+    print(f"Date   : {selfTest[2]}")
+    print(f"FSWTemp: {s.query('SOUR:TEMP:FRON?')}")
+
+    print('\n--Switch Count--')
     print(f"AC/DC  : {s.query('DIAG:INFO:CCO? ACDC')}")
     print(f"Attn5  : {s.query('DIAG:INFO:CCO? ATT5')}")
     print(f"Attn10 : {s.query('DIAG:INFO:CCO? ATT10')}")
@@ -13,8 +29,3 @@ if __name__ == "__main__":
     print(f"Attn40 : {s.query('DIAG:INFO:CCO? ATT40')}")
     print(f"Cal Src: {s.query('DIAG:INFO:CCO? CAL')}")
     print(f"Preamp : {s.query('DIAG:INFO:CCO? PRE')}")
-
-    HWInfo = s.query('DIAG:SERV:HWIN?').split('","')
-    for module in HWInfo:
-        if 'DETECTOR EXTENSION BOARD 2' in module:
-            print(module)
