172cc1e146e35aa01c2f75d27a819623d19c351a
Martin Lim <trabin9@gmail.com>
2021-04-26 09:01:53 -0700

    pylint space

--- src/iSck_FSW_5GNR_Get_AllocSummary.py
+++ src/iSck_FSW_5GNR_Get_AllocSummary.py
@@ -6,9 +6,9 @@ from iSocket import iSocket                 # Import socket module
 # #############################################################################
 FSW = iSocket().open('192.168.58.109', 5025)
 FSW.s.settimeout(5)
-data = FSW.query(':TRAC7:DATA? TRACE1').split(',')
+data = FSW.query(':TRAC4:DATA? TRACE1').split(',')
 for i, dat in enumerate(data):
-    print(f'{dat:7s} ',end='')
+    print(f'{dat:7s} ', end='')
     if i % 9 == 8:
         print('')
 print(len(data))
