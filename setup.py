81077064cd9dcf0a1f30d86c62b4df948b0c1079
Martin Lim <trabin9@gmail.com>
2021-05-28 13:54:05 -0700

    fix spacing

--- setup.py
+++ setup.py
@@ -18,33 +18,34 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     long_description = f.read()
 
-setup(name='covid',
-      version='2021.02.16',
-      description='Instrument Debug Tools',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      classifiers=[
+setup(
+    name='pyTools',
+    version='2021.02.16',
+    description='Instrument Debug Tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
         'Development Status :: 4 - Beta',      # 3:Alpha 4:Beta 5:Stable
         'License :: Freely Distributable',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 3.8',
-      ],
-      keywords='covid',
-      url='https://github.com/mclim9/RS_pyTools',
-      author='Martin Lim',
-      author_email='martin.lim@rsa.rohde-schwarz.com',
-      license='R&S Terms and Conditions for Royalty-Free Products',
-      packages=find_packages(exclude=['test', 'proto']),
-      install_requires=[
-      ],
-      test_suite='test',
-      include_package_data=True,
-      zip_safe=False,
-      entry_points={
+        'Programming Language :: Python :: 3.8'
+    ],
+    keywords='covid',
+    url='https://github.com/mclim9/RS_pyTools',
+    author='Martin Lim',
+    author_email='martin.lim@rsa.rohde-schwarz.com',
+    license='R&S Terms and Conditions for Royalty-Free Products',
+    packages=find_packages(exclude=['test', 'proto']),
+    install_requires=[
+    ],
+    test_suite='test',
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
         'console_scripts': [
             'covid=covid.bin.cli:main'
-            ],
-      },
+        ],
+    },
 )
