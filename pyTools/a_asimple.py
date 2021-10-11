8442b7d36de79d8bfde6614840a84e57cc0b1184
mclim-rsa25545 <martin.lim@rsa.rohde-schwarz.com>
2021-10-11 15:09:11 -0700

    debug

---
+++ pyTools/a_asimple.py
@@ -0,0 +1,10 @@
+""" Rohde & Schwarz Automation for demonstration use."""
+import socket
+
+s = socket.socket()
+s.connect(('192.168.58.114', 5025))
+s.settimeout(5)
+
+SCPI = '*IDN?'
+s.sendall(f'{SCPI}\n'.encode())         # Write SCPI
+sOut = s.recv(100000).decode().strip()  # Read socket
--- pyTools/a_debug.py
+++ pyTools/a_debug.py
@@ -1,27 +1,20 @@
 """ Rohde & Schwarz Automation for demonstration use."""
-import socket                               # Import socket module
+import socket
 
 def sQuery(SCPI):                           # Socket Query
     sWrite(SCPI)
-    sOut = s.recv(100000).decode().strip()  # Read socket
+    sOut = s.recv(100000).decode().strip()
     print(f'Query: {sOut}')
     return sOut
 
 
 def sWrite(SCPI):                           # Socket Write
     print(f'Write: {SCPI}')
-    s.sendall(f'{SCPI}\n'.encode())         # Write SCPI
+    s.sendall(f'{SCPI}\n'.encode())
 
-s = socket.socket()                         # Create a socket object
+s = socket.socket()
 s.connect(('192.168.58.114', 5025))
-s.settimeout(5)                             # Timeout in seconds
+s.settimeout(5)
 
-# sWrite('FORM:DATA ASCII')
-sWrite('FORM:DATA REAL,32')
-sWrite('TRAC:IQ:DATA:FORM IQP')
-# rdStr = sQuery(f':STAT:QUES:POW:COND?')
-# sWrite(':STAT:QUES:INT:ENAB 4')
-# rdStr = sQuery(f':STAT:QUES:INT:COND?')
-rdStr = sQuery(f'*IDN?')
-
-print(rdStr)
+sQuery(f'*IDN?')
+sQuery(f'*OPC?')
