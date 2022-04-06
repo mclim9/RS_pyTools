50df99ae84a19766ac80164483cd99268743c8e5
mclim-rsa25545 <martin.lim@rsa.rohde-schwarz.com>
2022-04-06 07:37:11 -0700

    added timing info

--- pyTools/iSck_VSG_File_In.py
+++ pyTools/iSck_VSG_File_In.py
@@ -1,20 +1,40 @@
-"""R&S Example code"""
 from iSocket import iSocket                 # Import socket module
+import timeit
+
+def findData(data, string):
+    strLen = len(string)
+    dstart = data.find(string)
+    begStr = data[dstart + strLen:dstart + 20]
+    cutStr = begStr[:begStr.find(b'}')]
+    return cutStr.decode()
 
 # #############################################################################
 # ## Code Begin
 # #############################################################################
-fileName = 'iSck_VSG_File_In.wv'
-
-K2      = iSocket().open('192.168.58.114', 5025)
+fileName = 'iSck_VSG_File_In-NR-FR1-TM1_1.wv'
 f = open(fileName, 'rb')
 data    = f.read()
 f.close()
 
-# data = b'hello'
 numBytes = len(data)
 numHead  = len(str(numBytes))
+
+K2       = iSocket().open('192.168.58.115', 5025)
 SCPI     = f'MMEM:DATA "/var/user/{fileName}",#{numHead}{numBytes}'
 command  = bytes(SCPI, 'utf-8') + data
+tick = timeit.default_timer()
 K2.writeBin(command)
-asdf = K2.query(':SYST:ERR?')
+testtime = timeit.default_timer() - tick
+
+samples  = int(findData(data, b'SAMPLES:'))
+clock    = int(findData(data, b'CLOCK:'))
+print(f'Error   : {K2.query(":SYST:ERR?")}')
+print(f'File    : {fileName}')
+print(f'Samples : {samples}')
+print(f'Clock   : {clock}')
+print(f'Duration: {samples / clock * 1000} msec')
+print(f'Transfer: Binary Transfer')
+print(f'')
+print(f'NumBytes: {numBytes}')
+print(f'Time    : {testtime:.3f} Sec')
+print(f'Thrput  : {numBytes/testtime/1e6:.3f} MB /sec')
