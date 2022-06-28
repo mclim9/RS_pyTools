e79b9939337080986a171dee7be4d5bc04125085
mclim-rsa25545 <martin.lim@rsa.rohde-schwarz.com>
2022-06-28 15:54:04 -0700

    debug w/ Devin

--- pyTools/5GNR/iSck_VSA_Autolvl_5GNR.py
+++ pyTools/5GNR/iSck_VSA_Autolvl_5GNR.py
@@ -1,22 +1,26 @@
 """ Rohde & Schwarz Automation for demonstration use."""
 from iSocket import iSocket                 # Import socket module
+import timeit
 oldEVM = 0
 
 def get_Settings():
     freq = s.queryFloat(':SENS:FREQ:CENT?') / 1e9       # Center Frequency
     refl = s.queryFloat('DISP:TRAC:Y:SCAL:RLEV?')       # Reference Level
     attn = s.queryInt(':INP:ATT?')                      # Input Attn
-    # pamp = s.queryInt(':INP:GAIN:STAT?')                # Preamp
+    # pamp = s.queryInt(':INP:GAIN:STAT?')              # Preamp
     pamp = 9999
+    tick = timeit.default_timer()
     s.query('INIT:IMM;*OPC?')
     ovld = s.queryFloat(':FETC:CC1:ISRC:FRAM:SUMM:EVM:ALL:AVER?')            # Overload Status
     print(f'{freq}GHz,{refl:6.2f}dBm,{attn:2d}dB,PA{pamp},EVM:{ovld:.2f}')
+    timeDelta = timeit.default_timer() - tick
+    # print(f'TTime: {timeDelta:.6f} sec')
+
     return [freq, refl, attn, pamp]
 
 def Get_EVM_Stat():
     global oldEVM
     s.query('INIT:IMM;*OPC?')
-    # newEVM = float(s.query(':FETC:EVM:ALL:AVER?'))      # WLANAD
     newEVM = float(s.query(':FETC:CC1:ISRC:FRAM:SUMM:EVM:ALL:AVER?'))      # WLANAD
     delta = oldEVM - newEVM
     oldEVM = newEVM
@@ -33,31 +37,35 @@ def shift_attn(delta):
     s.write(f':INP:ATT {attn + delta}')
 
 def optimize_FrontEnd():
+    tick = timeit.default_timer()
     s.write('INIT:CONT OFF')
     s.write(f':INP:ATT:AUTO OFF')
-    s.write(f':DISP:TRAC:Y:SCAL:RLEV 0')
-
-    # s.write(':CONF:POW:AUTO ONCE;*WAI')               # 802.11 AL
-    # s.write(':SENS:ADJ:LEV;*WAI')                     # Spec; 5GNR; IQ_Analyzer AL
+    s.write(f':INP:ATT 0')
+    # s.write(f':DISP:TRAC:Y:SCAL:RLEV 0')
+    # s.write(':SENS:ADJ:LEV;*WAI')                         # Spec; 5GNR; IQ_Analyzer AL
+    chPwr = s.queryFloat(f':FETC:CC1:ISRC:FRAM:SUMM:POW:AVER?')  # Get Power(dBm)
+    s.write(f':DISP:TRAC:Y:SCAL:RLEV {chPwr + 12}')
     get_Settings()
-
-    num_loop = 0
-    while (Get_EVM_Stat() > 0) and (num_loop < 20):
-        shift_RefLvl(-1)
-        if get_Settings()[1] > 30:                      # Ref Level
-            break
-        num_loop += 1
-    shift_RefLvl(1)
 
     end_attn_delta = -1
     while Get_EVM_Stat() > 0:
-        shift_attn(1)
+        shift_attn(2)
         if (get_Settings()[2] == 0):                    # Get Attn
             end_attn_delta = 0
             break
     shift_attn(end_attn_delta)
     get_Settings()
 
+    num_loop = 0
+    while (Get_EVM_Stat() > 0) and (num_loop < 10):
+        shift_RefLvl(-2)
+        if get_Settings()[1] > 30:                      # Ref Level
+            break
+        num_loop += 1
+    shift_RefLvl(1)
+    timeDelta = timeit.default_timer() - tick
+    print(f'TTime: {timeDelta:.6f} sec')
+
 if __name__ == "__main__":
-    s = iSocket().open('192.168.58.109', 5025)
+    s = iSocket().open('192.168.10.40', 5025)
     optimize_FrontEnd()
